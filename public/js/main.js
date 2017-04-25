a4b1a89d783bd9787cd80f463ea1a00f95166d63
Eric Abenojar <abenoeric@gmail.com>
2017-04-24 17:01:28 -0700

    fixed jqeury file

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,19 @@
-<script>
+
 $("#about").click(function() {
   console.log('clickedddddd');
   $('html, body').animate({
   scrollTop: $(".row3").offset().top
-  }, 2000);
+}, 1500);
   });
 $("#projects").click(function() {
     console.log('clickedddddd');
     $('html, body').animate({
     scrollTop: $(".row4-block").offset().top
-    }, 2000);
+  }, 1500);
     });
 $("#contact").click(function() {
         console.log('clickedddddd');
         $('html, body').animate({
         scrollTop: $(".row5").offset().top
-        }, 2000);
+      }, 1500);
         });
-</script>
